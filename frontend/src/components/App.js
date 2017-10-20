d324f153d763dcf427de3fb661972fd01a53128a
Cody Boyko <codywboyko@gmail.com>
2017-10-20 20:02:50 +0000

    wip: Make basic call and list out categories.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,25 +4,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      backend: 'backend-data'
+      backend: {categories: []}
     }
   }
 
   componentDidMount() {
     const url = `${process.env.REACT_APP_BACKEND}/categories`;
-    console.log('fetching from url', url);
     fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
                  credentials: 'include' } )
-      .then( (res) => { return(res.text()) })
-      .then((data) => {
-        this.setState({backend:data});
+      .then( (res) => { 
+        return(res.json()) 
+      })
+      .then((data) => {        
+        this.setState({backend: data});
       });
   }
 
   render() {
     return (
-      <div>
+      <div>        
         <h1>Readable</h1>
+        <p>Categories include:</p> 
+        <ul>          
+          { this.state.backend.categories.map((category) => (
+            <li key={category.name}><a href={category.path}>{category.name}</a></li>
+          )) }
+        </ul>
       </div>
     );
   }
